55bbc50cbb6b2f8353a70fe251a0ab1020236ab5
alfie <alfieroddan@gmail.com>
2024-09-11 18:52:53 +0100

    simple template

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
+import argparse
+import cv2
+
+
 def main():
-    print("test")
+    parser = argparse.ArgumentParser(
+        description="Simple point and click and record coordinates in image"
+    )
+    parser.add_argument("path", help="Description for foo argument")
+    args = parser.parse_args()
+
+    # image path
+    image_path = args.path
+    image = cv2.imread(image_path)
+
+    cv2.namedWindow('Image', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
